53861df06f0413d646eab671cdeb949c2c8b4998
Fahrur Rozi <rfahrur6054@gmail.com>
2024-07-15 22:31:43 +0700

    fix skill

--- src/app/components/skillSection/page.js
+++ src/app/components/skillSection/page.js
@@ -39,7 +39,7 @@ const SkillSection = () => {
 
   return (
     <section id="skills" className="py-16 bg-gray-400">
-      <div className="max-w-7xl mx-96 px-4 sm:px-6 lg:px-8">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <h2 className="text-3xl font-extrabold text-gray-900 sm:text-4xl mb-8 text-center">
           Skills
         </h2>
