9f181114f09bff1ba28cd4db5813e2f651c1af3a
Fahrur Rozi <rfahrur6054@gmail.com>
2024-07-19 15:11:15 +0700

    add new branchfr

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,11 @@
 // src/app/page.js
 
 import Navbar from "./components/navbar/page";
-import HeroSection from "./components/heroSection";
+import HeroSection from "./components/heroSection/heroSection";
 import ProjectSection from "./components/carousel/page";
 import ExperienceSection from "./components/ExperienceSection/page";
 import SkillSection from "./components/skillSection/page";
-import ContactSection from "./components/contactSection";
+import ContactSection from "./components/contactSection/page";
 
 export default function Home() {
   return (
